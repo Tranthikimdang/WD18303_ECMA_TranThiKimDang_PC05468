af89463f18eaea674764a2832d4732a4136886f7
Tranthikimdang <dangttkpc05468@fpt.edu.vn>
2024-01-10 20:29:48 +0700

    lab2

---
+++ bai3/script.js
@@ -0,0 +1,7 @@
+const array1 = ["jan", "feb", "mar", "apr", "may"];
+let array2 = [...array1, "JUN", "JUL", "SEP"];
+
+console.log("Array1: ");
+console.log(array1);
+console.log("Array2: ");
+console.log(array2);
